b551c3d669e136c6c128dcb5f92328ed033895ef
Qaseem Ameen <qaseemameen100@gmail.com>
2025-07-28 02:06:30 +0300

    fix: enable CORS3

--- server/index.js
+++ server/index.js
@@ -25,12 +25,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // middlewares
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
 app.use(cors({
   origin: 'http://localhost:3000', // 👈 أو 'https://اسم_موقعك.رابط'
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders:['Content-Type','Authorization'],
   credentials: true,
 }));
 app.options('*',cors());
