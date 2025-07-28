411955d7eb6bb9c1318ca34eae1c1d76cec2d7c9
Qaseem Ameen <qaseemameen100@gmail.com>
2025-07-29 02:37:29 +0300

    add delete act1

--- index.js
+++ index.js
@@ -272,7 +272,7 @@ app.get('/api/lessons', async (req, res) => {
 });
 
 // DELETE /api/activities/:id
-router.delete('/:id', (req, res) => {
+api.delete('/api/activities/:id', (req, res) => {
   const { id } = req.params;
 
   const sql = 'DELETE FROM activities WHERE id = ?';
